5bc7b93e5340596ea5d794a3fce775ee7d68ea71
rehman2711 <abdul.r@jaguarsolutions.co.in>
2026-03-29 11:26:29 +0530

    PORT env name updated to DB_PORT

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
